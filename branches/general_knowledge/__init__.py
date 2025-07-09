30dcf2504ce23cb1861c22ef51268c8df563883c
bendinglee <bennie08dingle@gmail.com>
2025-07-09 17:06:55 +0100

    Update __init__.py

--- branches/general_knowledge/__init__.py
+++ branches/general_knowledge/__init__.py
@@ -1,1 +1,11 @@
-# General Knowledge Plugin Initialization
+from .routes import knowledge_api
+
+BRANCH_NAME = "General Knowledge"
+BRANCH_VERSION = "1.0.0"
+BRANCH_DESCRIPTION = "Answers factual questions based on JSON modules"
+
+def initialize_branch():
+    print(f"📖 Initializing {BRANCH_NAME} v{BRANCH_VERSION}")
+    return True
+
+__all__ = ["knowledge_api", "initialize_branch"]
