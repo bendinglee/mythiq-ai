fe7d1a343c2481de6fa6ae6df4cbea64bb6b4a6d
bendinglee <bennie08dingle@gmail.com>
2025-07-09 17:28:50 +0100

    Create style_map.py

---
+++ branches/image_generator/prompt_preview/style_map.py
@@ -0,0 +1,8 @@
+def get_available_styles():
+    return {
+        "cinematic": "Ultra detailed, wide angle, 8K",
+        "anime": "Studio Ghibli look, cel-shaded",
+        "fantasy": "Ethereal, glowing particles",
+        "photorealistic": "Hyperreal, 35mm, cinematic shadows",
+        "comic": "Bold contrast, inked outlines"
+    }
