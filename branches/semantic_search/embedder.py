56dd673359aa58a70694f9b89ceb75266db988d7
bendinglee <bennie08dingle@gmail.com>
2025-07-09 01:28:20 +0100

    Update embedder.py

--- branches/semantic_search/embedder.py
+++ branches/semantic_search/embedder.py
@@ -1,12 +1,18 @@
 from sentence_transformers import SentenceTransformer
 
 _model = None
+
 def get_embedder():
     global _model
     if _model is None:
-        _model = SentenceTransformer("all-MiniLM-L6-v2")
+        _model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
     return _model
 
 def embed_text(text):
     model = get_embedder()
     return model.encode([text])[0]
+
+def embed_texts(texts, model=None):
+    if model is None:
+        model = get_embedder()
+    return model.encode(texts, convert_to_tensor=True)
