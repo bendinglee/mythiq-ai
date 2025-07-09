881dcd88d8af822b6881de38f6dc7dd794ff92ac
bendinglee <bennie08dingle@gmail.com>
2025-07-09 21:53:29 +0100

    Update __init__.py

--- branches/math_solver/__init__.py
+++ branches/math_solver/__init__.py
@@ -1,1 +1,10 @@
+from .router import math_solver_route
 
+BRANCH_NAME = "Math Solver"
+BRANCH_VERSION = "1.0.0"
+
+def initialize_branch():
+    print(f"➗ Initializing {BRANCH_NAME} v{BRANCH_VERSION}")
+    return True
+
+__all__ = ["math_solver_route", "initialize_branch"]
