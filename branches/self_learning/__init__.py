d941ae741feef7ffa5ca407541370c6094f528d4
bendinglee <bennie08dingle@gmail.com>
2025-07-10 01:05:08 +0100

    Update __init__.py

--- branches/self_learning/__init__.py
+++ branches/self_learning/__init__.py
@@ -1,1 +1,10 @@
+from .routes import self_learning_api
 
+BRANCH_NAME = "Self Learning"
+BRANCH_VERSION = "1.0.0"
+
+def initialize_branch():
+    print(f"🧠 Booting {BRANCH_NAME} v{BRANCH_VERSION}")
+    return True
+
+__all__ = ["self_learning_api", "initialize_branch"]
