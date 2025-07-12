48e2d455144521041cb1c72c7c524bbe7a9fcadd
bendinglee <bennie08dingle@gmail.com>
2025-07-12 22:33:44 +0100

    Delete branches/creator_mode/tutorial_mode directory

--- branches/creator_mode/tutorial_mode/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify
-from branches.tutorial_mode.tutorial_renderer import render_tutorial
-
-tutorial_api = Blueprint("tutorial_mode", __name__)
-
-@tutorial_api.route("/tutorial-mode", methods=["GET"])
-def tutorial_mode():
-    result = render_tutorial()
-    return jsonify(result)
