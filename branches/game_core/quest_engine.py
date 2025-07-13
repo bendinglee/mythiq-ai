b49cc18fea03c19584278d927adf107edf5210b6
bendinglee <bennie08dingle@gmail.com>
2025-07-13 19:51:10 +0100

    Create quest_engine.py

---
+++ branches/game_core/quest_engine.py
@@ -0,0 +1,10 @@
+def generate_quest(title="Reclaim the Archive"):
+    return {
+        "title": title,
+        "objectives": [
+            "Find the memory shard",
+            "Decode its embedded signal",
+            "Return it to the reflector"
+        ],
+        "reward": "Ability to rewrite history"
+    }
